e1cfc6300d5b5f4c72b14cb8fdd9dbfd162e6ee4
emmett-create <emmett@agency-eight.com>
2026-05-26 12:39:07 -0400

    Create config.js

---
+++ docs/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://rieakopaagjwbueghsju.supabase.co';                                                                                                                                          
+  const SUPABASE_KEY = 'sb_publishable_BmVmEjYrHQUTJxTUKZHfUQ_PPfnQAWy';
