d06ad3506cb78d0178f0ca834e4d9ecc11e6eca1
emmett-create <emmett@agency-eight.com>
2026-05-27 15:06:43 -0400

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,2 @@
 const SUPABASE_URL = 'https://unfjrmqfclrvkeqplnvn.supabase.co'                                                                                                                                           
-  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuZmpybXFmY2xydmtlcXBsbnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDIwOTUsImV4cCI6MjA5NTQ3ODA5NX0.aSkOmTyaXHfk5dY
-  B6scNKEVOSgJOo6AS1xdkRYqb2_o'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuZmpybXFmY2xydmtlcXBsbnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDIwOTUsImV4cCI6MjA5NTQ3ODA5NX0.aSkOmTyaXHfk5dYB6scNKEVOSgJOo6AS1xdkRYqb2_o'
